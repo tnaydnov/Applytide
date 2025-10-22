f7bb4dc33b2cca178b6acdba22b1131e8e4511be
tomernay <tomernay@post.bgu.ac.il>
2025-10-22 10:37:52 +0300

    error fix

--- backend/app/db/migrations/versions/20251022_merge_branches.py
+++ backend/app/db/migrations/versions/20251022_merge_branches.py
@@ -1,8 +1,8 @@
-"""merge heads
+"""merge branches after admin cleanup
 
-Revision ID: h8i9j0k1l2m3
-Revises: c5d6e7f8g9h0, g3h4i5j6k7l8
-Create Date: 2025-10-05 12:00:00.000000
+Revision ID: 20251022_merge
+Revises: 20251021_archive, g3h4i5j6k7l8
+Create Date: 2025-10-22 00:00:00.000000
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'h8i9j0k1l2m3'
-down_revision = ('c5d6e7f8g9h0', 'g3h4i5j6k7l8')
+revision = '20251022_merge'
+down_revision = ('20251021_archive', 'g3h4i5j6k7l8')
 branch_labels = None
 depends_on = None
 
